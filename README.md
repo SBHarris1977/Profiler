07e1cbb4f8bb5fbc25dbe3658a6048644aa773d9
Serina Harris <sbharris1977@gmail.com>
2019-12-17 00:54:16 -0600

    fixed broken code in command line application, can now generate a pdf

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Profiler
 
-Profiler is a command line application that creates a pdf resume of your GitHub profile.
-I used the following libraries: inquirer, puppeteer, and handlebars.
+Profiler is a command line application that creates a pdf resume of your GitHub profile by asking a few key questions.
+I used the following libraries: inquirer, html-pdf and axios.
 
-Inquirer - was used to allow for the creation of the command line application by asking questions and providing error feedback.
 
-Puppeteer - makes it easy to generate a pdf from the page content
+NOTE: Had several issues trying to get this to work. Turning in assignment, but will resolve later. 
 
+Able o resolve the broken code 12/17 and pdf is now generated.
--- pdf-generator.js
+++ pdf-generator.js
@@ -1,5 +1,79 @@
-const inquirer = require("inquirer");
 const fs = require("fs");
-const path = require("path");
-const puppeteer = require("puppeteer");
-const handlebars = require("handlebars");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const pdf = require("html-pdf");
+var html = fs.readFileSync('./index.html', 'utf8'); 
+var options = {format: 'Letter'}
+
+
+
+inquirer
+.prompt([
+    {
+        message: "What is your first and last name?",
+        name: "name"
+    
+    },
+
+    {
+message: "Enter your github username",
+    name: "username"
+    
+},
+
+{
+    message: "What's your fave color?",
+    name: "color"
+}
+])
+.then(function({name, username, color}){
+    const queryURL = `https://api.github.com/users/${username}`;
+
+    axios
+    .get(queryURL)
+    .then(function(res){
+        // console.log(res.data);
+        var avatar_url = res.data.avatar_url;
+        var url = res.data.html_url;
+        var repo = res.data.public_repos;
+        var followers = res.data.followers;
+        var following = res.data.following;
+        var location = res.data.location;
+    
+    fs.writeFile("index.html", `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body style="background: ${color};">
+        <img src="${avatar_url}">
+        <h1>
+        <a href="${url}">github</a>
+        </h1>
+        <h2>Name: ${name}</h2>
+        <p>Number of Repos: ${repo}</p>
+        <p>Number of Followers: ${followers}</p>
+        <p>Number Following: ${following}</p>
+        <p>Location: ${location}</p>
+    </body>
+    </html>`, function(err){
+        if (err){
+            throw err;
+        }
+    })
+    
+     
+    
+     });
+})
+.then(function(){
+    pdf.create(html, options).toFile('./index.pdf', function(err, data){
+         if (err) {
+             throw err;
+         }
+         console.log(data);
+     }); 
+})
